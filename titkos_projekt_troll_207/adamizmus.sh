c3034298705a79746f6d37bcfd24cca6adbcbc14
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2024-11-15 18:45:06 +0100

    Create adamizmus.sh

---
+++ titkos_projekt_troll_207/adamizmus.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# chmod u+x adamizmus.sh
+
+EV=`date +%Y` # marhára megbukunk
+HO=`date +%m`
+NA=`date +%d`
+b="."
+
+for i in $(seq 1 5)
+do
+  echo -n "Loading"
+  printf "%0.s$b" + $(seq 1 $i) 
+  sleep 0.5
+  echo
+done
+
+echo
+echo "Urunk" $EV "esztendejében" $HO "indusán és" $NA "szent napján elekrkezett az idő hogy isten császárunk Ádám királyunk regéjét és áldását hitét terjesszük minél szélesebb körökben... Az idő elérkezett!"
+echo "Ervin atyánk elhagy minket lelke itt marad közöttünk! Addig is szeressük és tiszteljük a RIP prokokolt!"
+echo "-Jelenések könyve 16:69 " $EV"."$HO"."$NA"."
+
+git clone https://github.com/loczylevi/json
+cd json/
+ls
+pwd
+
+utvonal="$(pwd)/adamizmus.jpg"
+
+gsettings set org.gnome.desktop.background picture-uri $utvonal
+gsettings set org.gnome.desktop.background picture-uri "file://$utvonal"
+gsettings set org.gnome.desktop.background picture-options "stretched"
+echo $utvonal
+echo "A háttérkép sikeresen beállítva:" $utvonal
+echo $utvonal

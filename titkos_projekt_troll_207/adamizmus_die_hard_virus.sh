c7aa0301c3f1c5fb164599f116b688dba7bb699e
Lóczy Levente Attila <71691777+loczylevi@users.noreply.github.com>
2024-11-15 21:33:18 +0100

    Update adamizmus_die_hard_virus.sh

--- titkos_projekt_troll_207/adamizmus_die_hard_virus.sh
+++ titkos_projekt_troll_207/adamizmus_die_hard_virus.sh
@@ -24,16 +24,17 @@ echo "-Jelenések könyve 16:69 " $EV"."$HO"."$NA"."
 echo "A vizuális ingercsokor bővitésére alkalmas képanyag eltarhálása (ellopása) a blaha.net.zanta.tv.hu domain-ről..."
 
 
-# Lista definiálása
 ip_cimek=("192.168.1.116" "192.168.1.102" "192.168.1.103" "192.168.1.104" "192.168.1.105")
 
 # For ciklus a lista elemein
 for ip in "${ip_cimek[@]}"; do
     echo "$ip"
-    sshpass -p 'password' ssh sis@$ip 'git clone https://github.com/loczylevi/json && gsettings set org.gnome.desktop.background picture-uri "file://$(pwd)/json/adamizmus.jpg" && gsettings set org.gnome.desktop.background picture-options "stretched"'
+    sshpass -p 'Macika_55' ssh sis@$ip 'git clone https://github.com/loczylevi/json && gsettings set org.gnome.desktop.background picture-uri "file://$(pwd)/json/adamizmus.jpg" && gsettings set org.gnome.desktop.background picture-options "stretched"'
     break
 
 
 done
 
 echo "blaha.net.zanta.tv.hu it kell legyél hogy elhidd..."
+
+
